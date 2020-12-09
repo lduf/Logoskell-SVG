a636dfd4d1959a45d65c112291983e401a6860c8
lduf <65276047+lduf@users.noreply.github.com>
2020-12-09 19:40:58 +0100

    Depot de la base du projet (data basique et tout le tralala) dans init.hs

---
+++ init.hs
@@ -0,0 +1,26 @@
+-- Datas
+-- -- Datas pour gérer le logoskell
+data Instruction = Forward Int | Left Int | Right Int | Repeat Int [Instruction] deriving(Show, Read)
+type Programme = [Instruction]
+
+-- -- Datas pour gérer le dessing / le svg
+data Coordonnee = Coordonnee Float Float deriving(Show, Read, Eq, Ord)
+type Angle = Float
+data Crayon  = Crayon Coordonnee Angle deriving(Show, Read, Eq, Ord)
+
+-- Variables
+color = "red"
+baseSVG = "<?xml version='1.0' encoding='utf-8'?> \n <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'> \n <title>Title</title> \n"
+svgLineEX = "<line x1='100.00' y1='100.00' x2='200.00' y2='100.00' stroke='"++color++"' />"
+
+-- Generation d'une ligne SVG avec des coordonnées
+svgLine :: (Coordonnee a) => a->a->String
+svgLine (Coordonnee x1 y1) (Coordonnee x2 y2) = "<line x1='"x1"' y1='"y1"' x2='"x2"' y2='"y2"' stroke='"++color++"' />"
+
+
+toSVG :: String -> String
+
+decodeInstruction :: String -> [Instruction]
+
+
+outputStrLst :: [String] -> IO()
