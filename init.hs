1e85f719908ade453a8dd4efa042930f890076af
lduf <65276047+lduf@users.noreply.github.com>
2020-12-10 16:20:35 +0100

    Programme fonctionnel

--- init.hs
+++ init.hs
@@ -1,3 +1,4 @@
+import System.IO
 import Prelude hiding (Left, Right)
 -- Datas
 -- -- Datas pour gérer le logoskell
@@ -13,10 +14,9 @@ data Crayon  = Crayon Coordonnee Angle deriving(Show, Read, Eq, Ord)
 color = "red"
 title = "Title"
 baseSVG = "<?xml version='1.0' encoding='utf-8'?> \n <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'> \n <title>"++title++"</title> \n"
-svgLineEX = "<line x1='100.00' y1='100.00' x2='200.00' y2='100.00' stroke='"++color++"' />"
 regexLogoskell ="^\\[.*\\]$"
-svg = ""
-mathis = Crayon (Coordonnee 0 0) 0
+svg = ""++baseSVG
+crayon = Crayon (Coordonnee 100 100) $ toradian 0
 -- Coordonnee ajout distance crayon
 newCrayon :: Crayon -> Float -> Angle-> Int-> Crayon
 newCrayon (Crayon coo@(Coordonnee x y) a) dist angle action
@@ -41,27 +41,26 @@ logo2hask :: String -> Programme
 logo2hask str = read str :: Programme
 
 toCrayon :: Programme -> Crayon -> [Crayon]
-toCrayon [] _ = [] 
+toCrayon [] c = [c] 
 toCrayon (x:xs) c = case x of
  (Forward v) -> c:toCrayon xs (newCrayon c v (toradian 0) 0)
  (Left a) -> c:toCrayon xs (newCrayon  c 0  (toradian a) 1)
  (Right a) -> c:toCrayon xs (newCrayon c 0 (toradian (-a)) 1)
- (Repeat n prog) ->  toCrayonrepeat prog n c ++ toCrayon xs c --doute
-
+ (Repeat n prog) ->  c:toCrayon xxs c 
+  where xxs = (take (n*length prog)  (cycle prog))++xs
 
 toCrayonrepeat :: Programme -> Int -> Crayon -> [Crayon]
-toCrayonrepeat _ 0 c = [c] -- doute
+toCrayonrepeat _ 0 c = [c] 
 toCrayonrepeat prog n c = (toCrayon prog c) ++ toCrayonrepeat prog (n-1) c
 
--- toSVG :: Programme -> String
--- toSVG [] = svg++""
--- toSVG (x:xs) = case x of 
---  (Forward v) -> (svgLine crayon $ newCrayon crayon v $ toradian 0)++toSVG xs
---  (Left a) -> (svgLine crayon $ newCrayon crayon 0 $ toradian a)++toSVG xs
---  (Right a) -> (svgLine crayon $ newCrayon crayon 0 $ toradian (-a))++toSVG xs
---  (Repeat n prog) -> (toSVGrepeat prog n)++ toSVG xs
-
 toSVG :: [Crayon] -> String
-toSVG c@(c0:c1:cs)
- | length c == 2 = svgLine c0 c1 
- | otherwise = (svgLine c0 c1)++toSVG (c1:cs)
+toSVG crayons = svg ++  toSVG' crayons
+
+toSVG' :: [Crayon] -> String
+toSVG' c@(c0:c1:cs)
+ | length c == 2 =  svgLine c0 c1 ++ "</svg>" 
+ | otherwise = (svgLine c0 c1)++toSVG' (c1:cs)
+
+main = do 
+  content <- hGetLine stdin
+  hPutStr stdout $ toSVG $ toCrayon (logo2hask content) crayon 
