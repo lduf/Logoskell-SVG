aa3ea947594cbd7b24d34b11a9ba00d8f72cfec0
lduf <65276047+lduf@users.noreply.github.com>
2020-12-10 12:07:56 +0100

    Correction de init.hs et base de la resolution du probleme

--- init.hs
+++ init.hs
@@ -1,7 +1,7 @@
 -- import Text.Regex
 -- Datas
 -- -- Datas pour gérer le logoskell
-data Instruction = Forward Int | Left Int | Right Int | Repeat Int [Instruction] deriving(Show, Read)
+data Instruction = Forward Float | Left Float | Right Float | Repeat Float [Instruction] deriving(Show, Read)
 type Programme = [Instruction]
 
 -- -- Datas pour gérer le dessing / le svg
@@ -11,36 +11,38 @@ data Crayon  = Crayon Coordonnee Angle deriving(Show, Read, Eq, Ord)
 
 -- Variables
 color = "red"
-baseSVG = "<?xml version='1.0' encoding='utf-8'?> \n <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'> \n <title>Title</title> \n"
+title = "Title"
+baseSVG = "<?xml version='1.0' encoding='utf-8'?> \n <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='400' height='400'> \n <title>"++title++"</title> \n"
 svgLineEX = "<line x1='100.00' y1='100.00' x2='200.00' y2='100.00' stroke='"++color++"' />"
 regexLogoskell ="^\\[.*\\]$"
-svg = ""
+svg = ""++baseSVG
 
 -- Coordonnee ajout distance crayon
-newCrayon :: Crayon -> Float -> Float-> Crayon
-newCrayon (Crayon (Coordonnee x y) (Angle a)) dist 0 = Crayon (Coordonnee (x + dist*(cos a)) (y + dist*(sin a))) Angle a
-newCrayon (Crayon coo (Angle a)) 0 angle = Crayon coo (Angle (a+angle))
+newCrayon :: Crayon -> Float -> Angle-> Crayon
+newCrayon (Crayon (Coordonnee x y) (Angle a)) dist 0 = Crayon (Coordonnee (x + dist*(cos a)) (y + dist*(sin a))) Angle a --  pour un x vérifier les modulations de pi
+newCrayon (Crayon coo (Angle a)) 0 angle = Crayon coo (Angle (a+angle)) -- Pour un left ou right
 
 -- Generation d'une ligne SVG avec des coordonnées
 
 svgLine :: Crayon -> Crayon ->String
-svgLine (coo1 (Angle _)) (coo2 (Angle _)) = svgLinecoo coo1 coo2 
-
-
-svgLinecoo :: Coordonnee -> Coordonnee ->String
-svgLinecoo (Coordonnee x1 y1) (Coordonnee x2 y2) = "<line x1='"++ show x1 ++"' y1='"++ show y1 ++"' x2='"++ show x2 ++"' y2='"++ show y2 ++"' stroke='"++color++"' /> \n"
+svgLine (coo1 _) (coo2 _) = svgLinecoo coo1 coo2 
 
 svgLinecoo :: Coordonnee -> Coordonnee ->String
 svgLinecoo (Coordonnee x1 y1) (Coordonnee x2 y2) = "<line x1='"++ show x1 ++"' y1='"++ show y1 ++"' x2='"++ show x2 ++"' y2='"++ show y2 ++"' stroke='"++color++"' /> \n"
 
+
+-- Verif du langage logoskell
 logo2hask :: String -> Programme
 logo2hask str = read str :: Programme
 
 toSVG :: Programme -> String
-toSVG [] = ""
-toSVG (x:xs) = case x of (Forward v) -> svgLine Crayon NewCrayon
+toSVG [] = svg++""
+toSVG (x:xs) = case x of 
+ (Forward v) -> (svgLine Crayon $ NewCrayon Crayon v 0)++toSVG xs
+ (Left a) -> (svgLine Crayon $ NewCrayon Crayon 0 a)++toSVG xs
+ (Right a) -> (svgLine Crayon $ NewCrayon Crayon 0 (-a)++toSVG xs
+ (Repeat n prog) -> toSVGrepeat prog n
 
--- decodeInstruction :: String -> [Instruction]
--- decodeInstruction  = 
-
--- outputStrLst :: [String] -> IO()
+toSVGrepeat :: Programme -> Int -> String
+toSVGrepeat _ 0 = ""
+toSVGrepeat prog n = (toSVG prog)++toSVGrepeat prog (n-1)
